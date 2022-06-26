465dd86a28262fd6e394ba248cfff8e67c632b99
Orodje Wisdom <69108713+hiWisdom@users.noreply.github.com>
2022-06-26 12:39:00 +0100

    post

--- main.js
+++ main.js
@@ -15,10 +15,9 @@ updateYear.innerHTML = newYearDate;
 
 //text animation code
 
-const writing_animation_one = document.querySelector(".third_text");
 const writing_animation_two = document.querySelector(".second_text");
-	
-
+const writing_animation_one = document.querySelector(".third_text");
+const writing_animation_three = document.querySelector(".forth_text");
 
 
 const writing_animation_one_Loaded = ()=> {
@@ -30,7 +29,7 @@ const writing_animation_one_Loaded = ()=> {
     },0 );
 
     setTimeout( ()=> {
-        writing_animation_one.textContent = "Som";
+        writing_animation_one.textContent = "Je suis";
     },4000 );
 
     setTimeout( ()=> {
@@ -50,15 +49,15 @@ setInterval(writing_animation_one_Loaded, 12000);
 	const writing_animation_two_Loaded = ()=> {
 		
 		setTimeout( ()=> {
-			writing_animation_two.textContent = "CODE";
+			writing_animation_two.textContent = "Code, Create and Develope";
 		},0 );
 	
 		setTimeout( ()=> {
-			writing_animation_two.textContent = "CREATE";
+			writing_animation_two.textContent = "Code, Creer, Developper";
 		},4000 );
 	
 		setTimeout( ()=> {
-			writing_animation_two.textContent = "DEVELOPE";
+			writing_animation_two.textContent = "Code, Schaffen, Entwickeln";
 		},8000 );
 	
 	}
@@ -67,3 +66,42 @@ setInterval(writing_animation_one_Loaded, 12000);
 	writing_animation_two_Loaded();
 	
 	setInterval(writing_animation_two_Loaded, 12000);
+
+
+
+
+
+	
+	const writing_animation_three_Loaded = ()=> {
+		
+		setTimeout( ()=> {
+			writing_animation_three.textContent = "Solutions";
+		},0 );
+	
+		setTimeout( ()=> {
+			writing_animation_three.textContent = "Designs";
+		},4000 );
+	
+		setTimeout( ()=> {
+			writing_animation_three.textContent = "Experience";
+		},8000 );
+	
+	}
+		
+		
+	writing_animation_three_Loaded();
+	
+	setInterval(writing_animation_three_Loaded, 12000);
+
+
+
+	//modal 
+	// Get the login modal
+var login_modal = document.getElementById('login_mode');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
