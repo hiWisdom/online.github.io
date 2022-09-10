84c2c930697c6259ebb7d8020dd2d652edff8dae
Orodje Wisdom <69108713+hiWisdom@users.noreply.github.com>
2022-09-10 22:20:05 +0100

    post

--- main.js
+++ main.js
@@ -363,4 +363,20 @@ window.addEventListener("load", function(){
 
 
 
+				//accordion programming
+				const accordion = document.getElementsByClassName("accordion");
+				for (i = 0; i < accordion.length; i++) {
+					accordion[i].addEventListener("click", function() {
+					this.classList.toggle("active");
+					const panel = this.nextElementSibling;
+					if (panel.style.display === "block") {
+					panel.style.display = "none";
+					} else {
+					panel.style.display = "block";
+					}
+				});
+				}
+
+
+
 
